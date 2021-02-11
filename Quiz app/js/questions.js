39bf5d212c7d496676b8335acf933d677db9903e
AS-Groop <git@github.com>
2021-02-11 15:53:37 +0500

    Test 2

---
+++ Quiz app/js/questions.js
@@ -0,0 +1,58 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+	{
+		numb:1,
+		question:"What does HTML atand for?",
+		answer:"Hyper Text Markup Languaga",
+		options:[
+		"Hyper Text Preprocessor",
+		"Hyper Text Markup Languaga",
+		"Hyper Text Multiple Language",
+		"Hyper Tool Multi Language"
+		]
+	},
+	{
+		numb:2,
+		question:"What does CSS stand for?",
+		answer:"Cascading Style Sheet",
+		options:[
+		"Common Style Sheet",
+		"Colorful Style Sheet",
+		"Computer Style Sheet",
+		"Cascading Style Sheet"
+		]
+	},
+	{
+		numb:3,
+		question:"What does PHP atand for?",
+		answer:"Hypertext Preprocessor",
+		options:[
+			"Hypertext Preprocessor",
+			"Hypertext Programming",
+			"Hypertext Preprogramming",
+			"Hometext Preprocessor"
+		]
+	},
+	{
+		numb:4,
+		question:"What does SQL atand for?",
+		answer:"Structure Query Language",
+		options:[
+		"Stylish Question Language",
+		"Stylesheet Query Language",
+		"Statement Question Language",
+		"Structure Query Language"
+		]
+	},
+	{
+		numb:5,
+		question:"What does XML atand for?",
+		answer:"eXtensible Markup Language",
+		options:[
+		"eXtensible Markup Language",
+		"eXecutable Multiple Language",
+		"eXtra Multi-Program Language",
+		"eXamine Multiple Language",
+		]
+	},
+];
